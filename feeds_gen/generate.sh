6dfb647dfaa5fd7d91190535b8df6cea38b86638
Jayesh Patel <umasolution@gmail.com>
2022-05-02 16:09:55 -0400

    Fixed latest build name

--- feeds_gen/generate.sh
+++ feeds_gen/generate.sh
@@ -4,5 +4,5 @@ date_now=$(date "+%F-%H-%M-%S")
 cp application.config /var/DB/feeds/application/
 tar -cvf /tmp/niahfeed_$date_now.tar /var/DB/CVEs /var/DB/feeds
 gzip /tmp/niahfeed_$date_now.tar
-tar -cvf /tmp/latest.tar /var/DB/CVEs /var/DB/feeds
-gzip /tmp/latest.tar
+tar -cvf /tmp/niahfeed_latest.tar /var/DB/CVEs /var/DB/feeds
+gzip /tmp/niahfeed_latest.tar
