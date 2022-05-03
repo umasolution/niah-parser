7091475ba3d757710212b786c9f85f8f70dedbf0
jays patel <umasolution@gmail.com>
2022-05-03 09:40:46 -0400

    Update generate.sh

--- feeds_gen/generate.sh
+++ feeds_gen/generate.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+rm -rf /tmp/niahfeed_latest.tar.gz
 date_now=$(date "+%F-%H-%M-%S")
 cp application.config /var/DB/feeds/application/
 tar -cvf /tmp/niahfeed_$date_now.tar /var/DB/CVEs /var/DB/feeds
